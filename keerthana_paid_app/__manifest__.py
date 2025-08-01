a557c48c8dedbf1bd949904281f4d9956f2f9949
keerthanachundi <keerthanachundi9@gmail.com>
2025-08-01 08:46:29 +0530

    Initial commit with paid module

---
+++ keerthana_paid_app/__manifest__.py
@@ -0,0 +1,16 @@
+
+{
+    'name': 'Keerthana Paid App',
+    'version': '17.0.1.0.0',
+    'category': 'Tools',
+    'summary': 'A sample paid app by Keerthana',
+    'description': 'This is a paid app uploaded to the Odoo App Store.',
+    'author': 'Keerthana',
+    'license': 'OPL-1',
+    'price' : 10.0,
+    'currency' : 'USD',
+    'depends': ['base'],
+    'data': ['views/sample_view.xml'],
+    'installable': True,
+    'application': True,
+}
