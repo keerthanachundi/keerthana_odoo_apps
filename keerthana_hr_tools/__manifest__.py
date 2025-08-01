e4bf66648cf9e68c47e8a6402a8373b42b253592
keerthanachundi <keerthanachundi9@gmail.com>
2025-08-01 10:58:07 +0530

    Initial commit with paid module test 2

---
+++ keerthana_hr_tools/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Keerthana HR Tools",
+    "version": "17.0.1.0.0",
+    "author": "Keerthana",
+    "license": "OPL-1",
+    "website": "https://yourcompany.com",
+    "summary": "Advanced HR features for Odoo 17",
+    "description": "Manage leave, attendance, onboarding and more.",
+    "category": "Human Resources",
+    "depends": ["hr"],
+    "data": ["views/hr_tools_view.xml"],
+    "installable": true,
+    "application": true,
+    "price": 79.00,
+    "currency": "USD"
+}
